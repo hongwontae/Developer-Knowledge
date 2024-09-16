951635909852f2bd63efc0b9795e33614b31eaa8
dnjsxoghd <feelchok1234@gmail.com>
2024-09-16 15:02:05 +0900

    +

--- JavaScript/test/test.js
+++ JavaScript/test/test.js
@@ -9,3 +9,14 @@ Promise.all([promise1, promise2, promise3]).then((result) => {
 Promise.allSettled([promise1, promise2, promise3]).then((result) => {
     console.log(result);
   });
+
+
+const a = [1,2,3];
+const b1 = [2,3,4];
+
+function testRest(...data){
+  console.log(data);
+}
+
+const b = testRest(a);
+console.log(b);
