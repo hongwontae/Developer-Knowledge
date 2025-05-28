02de63c59585d3ec5f5f11599ea358a4b2ba6d04
dnjsxoghd <feelchok1234@gmail.com>
2025-05-28 16:24:34 +0900

    cote

--- Coding-Test/Programmers/code/Basic-14/cote.js
+++ Coding-Test/Programmers/code/Basic-14/cote.js
@@ -7,3 +7,12 @@ function solution(n, k) {
 }
 console.log(solution(10, 3));
 console.log("koala3121".slice(-2))
+
+function solution2(arr, intervals) {
+    let arrs = [];
+     intervals.forEach(([start, end], idx, ar)=>{
+        return arrs.push(...arr.slice(start,end+1))
+    })
+    return arrs;
+}
+console.log(solution2([1, 2, 3, 4, 5], [[1, 3], [0, 4]]))
